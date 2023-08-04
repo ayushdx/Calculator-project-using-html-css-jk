80d98088ec80c3fae19a7cffeb0f892d5dd9c0ab
Ayush Dwivedi <78481630+ayushdx@users.noreply.github.com>
2023-08-04 22:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+
+Array.from(buttons).forEach(button => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      try {
+        // Use eval() to evaluate the expression and limit the result to 2 decimal places
+        string = eval(string).toFixed(2);
+      } catch (error) {
+        string = "Error";
       }
-  })
-})
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == 'C') {
+      string = "";
+      document.querySelector('input').value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
